e4f4b8675ba75ad686081e63119bfaa3f4a8eaf4
jaouad4 <salahjd16@gmail.com>
2025-10-05 20:17:17 +0100

    Create the Kafka Streams Function (Data Analytics)

--- src/main/java/ma/jaouad/handlers/PageEventHandler.java
+++ src/main/java/ma/jaouad/handlers/PageEventHandler.java
@@ -4,11 +4,18 @@ import ma.jaouad.events.PageEvent;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.Grouped;
 
 import java.util.Date;
 import java.util.Random;
+import java.time.Duration;
 import java.util.function.Supplier;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Component
 public class PageEventHandler {
@@ -30,5 +37,17 @@ public class PageEventHandler {
                 new Date(),
                 new Random().nextInt(10000) + 10
         );
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kstreamFunction() {
+        return input -> input
+                .filter((k, v) -> v.duration() > 100)
+                .map((k, v) -> new KeyValue<>(v.name(), v.duration()))
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofSeconds(5)))
+                .count()
+                .toStream()
+                .map((k, v) -> new KeyValue<>(k.key(), v));
     }
 }
