8e786539af202a915bf8dd01af1024c33ebe2659
jaouad4 <salahjd16@gmail.com>
2025-10-05 20:14:01 +0100

    Create the Consumer & Supplier

--- src/main/java/ma/jaouad/handlers/PageEventHandler.java
+++ src/main/java/ma/jaouad/handlers/PageEventHandler.java
@@ -1,4 +1,34 @@
 package ma.jaouad.handlers;
 
+import ma.jaouad.events.PageEvent;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.function.Consumer;
+
+@Component
 public class PageEventHandler {
+
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer() {
+        return input -> {
+            System.out.println("*******************");
+            System.out.println("Consuming: " + input.toString());
+            System.out.println("*******************");
+        };
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier() {
+        return () -> new PageEvent(
+                Math.random() > 0.5 ? "P1" : "P2",
+                Math.random() > 0.5 ? "U1" : "U2",
+                new Date(),
+                new Random().nextInt(10000) + 10
+        );
+    }
 }
